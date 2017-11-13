b29301db18d00547ddbbcd688c2fb01bf2c062c3
Hector Stark <Hector_Stark@github.com>
2017-11-13 17:29:25 -0600

    Fuentes
    Para guardar fuentes

---
+++ application/models/Fuentes.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Fuentes extends CI_Model {
+	public function __construct()
+	{
+		parent::__construct();
+		//Do your magic here
+	}
+
+	public function insertar_fuente($nombre_fuente='',$tipo_recurso='', $enlace='',$categoria='')
+	{
+		$datos = array('nombre_Fuente' => $nombre_fuente , 'tipo_Recurso' => $tipo_recurso , 'enlace' => $enlace ,'categoria' =>$categoria);
+	}
+	$query = $this->db->insert('Fuentes', $datos);
+		if (query){
+			return $this->db->insert_id();
+		} else{
+			return 0;
+		}
+	public function buscar_fuente_por_tipo_recurso($tipo_recurso)
+	{
+		$this->db->where('tipo_Recurso', $tipo_recurso);
+		$fuentes = $this->db->get('Fuentes')->row();
+
+
+	}
+	
+
+}
+
+/* End of file Fuentes.php */
+/* Location: ./application/models/Fuentes.php */
